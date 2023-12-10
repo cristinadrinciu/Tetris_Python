b31afeb91cfa6f63e048514264220171aaf5191e
Vasile Alexandru Gabriel <vasilealexandru37@gmai.com>
2023-12-10 22:08:18 +0200

    Added the UI : score, next_block, switchBG. TODO: play with colors

--- colors.py
+++ colors.py
@@ -7,8 +7,11 @@ class Colors:
     light_green = (102, 255, 102)
     deep_green = (0, 153, 0)
     intense_red = (255, 0, 0)
+    white = (255, 255, 255)
+    light_blue = (59, 85, 162)
 
     @classmethod
     def get_cell_colors(cls):
         return [cls.gray, cls.magenta, cls.orange, cls.lila,
-                cls.yellow, cls.light_green, cls.deep_green, cls.intense_red]
+                cls.yellow, cls.light_green, cls.deep_green,
+                cls.intense_red]
--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 import pygame, sys
 from game import Game
+from colors import Colors
 
 from blocks import *
 
 pygame.init()
-bgColor = (153, 204, 255)
+
+
+# Create title font
+title_font = pygame.font.SysFont("Arial", 30)
+
+# Create the score surface
+score_surface = title_font.render("Score" , True, Colors.white)
+
+# Create the next surface (to display the next block)
+next_surface = title_font.render("Next" , True, Colors.white)
+
+# Create the game over surface
+game_over_surface = title_font.render("Game Over", True, Colors.white)
+
+# Create the button text surface
+button_text = title_font.render("Switch", True, Colors.white)
+
+# (x, y, width, height)
+score_rect = pygame.Rect(320, 55, 170, 60)
+next_rect = pygame.Rect(320, 215, 170, 180)
+button = pygame.Rect(360, 500, 100, 60)
+
+dayColor = (153, 204, 255)
+nightColor = (0, 0, 0)
+
+bgColor = dayColor
 
 # Create the screen
-screen = pygame.display.set_mode((300, 600))
+screen = pygame.display.set_mode((495, 640))
 
 pygame.display.set_caption("Python Tetris")
 
@@ -51,8 +77,33 @@ while True:
         if event.type == GAME_UPDATE and game.game_over == False:
             game.move_down()
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if bgColor == dayColor:
+                bgColor = nightColor
+            else:
+                bgColor = dayColor
+
     # Set background color
     screen.fill(bgColor)
+
+    # Print score on screen
+    screen.blit(score_surface, (365, 20, 50, 50))
+
+    # Print next block on screen
+    screen.blit(next_surface, (375, 180, 50, 50))
+
+    if game.game_over == True:
+        screen.blit(game_over_surface, (328, 450, 60, 60))
+
+    # Print 
+    pygame.draw.rect(screen, Colors.light_blue, score_rect, 0, 100)
+    pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
+    pygame.draw.rect(screen, Colors.light_blue, button, 0, 10)
+
+    # Draw button text
+    button_text_rect = button_text.get_rect(center=button.center)
+    screen.blit(button_text, button_text_rect)
+
     game.draw(screen)
     
     # Update screen
